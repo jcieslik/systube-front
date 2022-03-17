dbae37f0c90392f18d026426b978bed4e469ca83
jcieslik <jakucie936@student.polsl.pl>
2022-03-17 23:24:05 +0100

    WIP changing resolution

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -10,5 +10,13 @@ export class VideoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let vid: HTMLVideoElement = document.getElementById("video") as HTMLVideoElement;
+    setTimeout(() => {
+      let currentTime = vid.currentTime;
+      vid.src = "https://localhost:7297/api/Video/GetFileById2"
+      vid.currentTime = currentTime;
+      vid.load();
+      vid.play();
+    }, 10000);
   }
 }
